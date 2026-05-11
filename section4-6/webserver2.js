6ff09c679b13c2dbbbd4d5512b011fb7aed61d50
노진경 <nojk0106@naver.com>
2026-05-11 11:18:39 +0000

    🐛 Fix: 안드로이드에서 테스트
    Fix: 안드로이드에서 테스트

--- section4-6/webserver2.js
+++ section4-6/webserver2.js
@@ -5,7 +5,7 @@ const port = 1337;
 
 var server = http.createServer(function(req, res){
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+  res.end('silly hyojung?\n');
 });
 
 server.listen(port, hostname, function(){
